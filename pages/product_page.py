22ef1ad0a55c6aba8487b89561c86ff73d0617ae
Hananou <hananoulutin@gmail.com>
2019-09-29 17:29:51 +0300

    added product page files

---
+++ pages/product_page.py
@@ -0,0 +1,6 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(browser)
+    def add_product_to_basket
+        
---
+++ test_product_page.py
@@ -0,0 +1,1 @@
+link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
