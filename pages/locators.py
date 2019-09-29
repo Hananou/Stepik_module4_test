9bb4542587734efe1e9ea58e1a1243706cf156f5
Hananou <hananoulutin@gmail.com>
2019-09-29 14:51:59 +0300

    added locators and asserts to login_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,16 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+#login_form
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_USERNAME = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    PASSWORD_RESET = (By.CSS_SELECTOR, "[href='/ru/password-reset/']")
+    LOGIN_SUBMIT = (By.CSS_SELECTOR, "#login_form > button")
+#register_form
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_EMAIL2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTRATION_SUBMIT = (By.CSS_SELECTOR, "#register_form > button")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,20 +1,23 @@
 from .base_page import BasePage
-
+from .locators import LoginPageLocators
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
         self.should_be_register_form()
-
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert True
-
+        #assert browser.current_url = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+        assert 'login' in browser.current_url, "Page addres does not contain text 'login'"
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
-
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_USERNAME), "Username field is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD), "Password field is not presented"
+        assert self.is_element_present(*LoginPageLocators.PASSWORD_RESET), "Password reset link is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_SUBMIT), "Login submit button is not presented"
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL), "Registration email field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL2), "Registration email2 field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_SUBMIT), "Registration submit button is not presented"
