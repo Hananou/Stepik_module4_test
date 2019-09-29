3844e42b0d4895321472b7e3f29da2ce40067dc6
Hananou <hananoulutin@gmail.com>
2019-09-29 17:27:37 +0300

    added password1 field test

--- pages/login_page.py
+++ pages/login_page.py
@@ -17,5 +17,6 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL), "Registration email field is not presented"
-        assert self.is_element_present(*LoginPageLocators.REGISTRATION_EMAIL2), "Registration email2 field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD1), "Registration password1 field is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_PASSWORD2), "Registration password2 field is not presented"
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_SUBMIT), "Registration submit button is not presented"
