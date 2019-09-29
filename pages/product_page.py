08204845f16c7cebe175000afdd8f5f79fa8f7a8
Hananou <hananoulutin@gmail.com>
2019-09-29 17:42:53 +0300

    updating product page test

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
-class ProductPage(browser)
+class ProductPage(BasePage)
     def add_product_to_basket
-        
+        add_to_busket_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        add_to_busket_link.click()
--- test_product_page.py
+++ test_product_page.py
@@ -1,1 +1,21 @@
+from selenium.common.exceptions import NoAlertPresentException
 link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+def solve_quiz_and_get_code(self):
+    alert = self.browser.switch_to.alert
+    x = alert.text.split(" ")[2]
+    answer = str(math.log(abs((12 * math.sin(float(x))))))
+    alert.send_keys(answer)
+    alert.accept()
+    try:
+        alert = self.browser.switch_to.alert
+        alert_text = alert.text
+        print(f"Your code: {alert_text}")
+        alert.accept()
+    except NoAlertPresentException:
+        print("No second alert presented")
+
+def test_guest_can_add_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    solve_quiz_and_get_code()
